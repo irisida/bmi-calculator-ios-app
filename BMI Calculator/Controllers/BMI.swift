7c1976d981c6c265449dd23dd81211c32d7774f3
irisida <20445648+irisida@users.noreply.github.com>
2021-04-11 14:30:33 +0100

    readme and proj files

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,16 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by ed on 05/04/2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
